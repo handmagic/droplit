04d0ee1219691caa00f94bb185b4f4258504d01c
HANDMAGIC Studio <order.ipan@gmail.com>
2025-12-23 09:10:52 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// DropLit Service Worker v0.6.0
-const CACHE_NAME = 'droplit-v0.6.0';
+// DropLit Service Worker v0.6.1
+const CACHE_NAME = 'droplit-v0.6.1';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
